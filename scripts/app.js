4939fdb5c8f97e85234c7551ef236afa1e16690f
dark0k_ <knezevicdarko899@gmail.com>
2023-01-21 10:48:07 +0100

    trash

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,9 @@
-buttonOne = document.getElementById("edit-player-1");
-butonTwo = document.getElementById("edit-player-2")
+const configOverlay = document.getElementById("config")
+
+const editButtonOne = document.getElementById("edit-player-1");
+const editButtonTwo = document.getElementById("edit-player-2");
+
+
+
+editButtonOne.addEventListener("click", editPlayer);
+editButtonTwo.addEventListener("click", editPlayer);
