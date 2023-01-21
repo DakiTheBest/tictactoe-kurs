79b2606db79f8e1dc70bc83c142c6f91c46fd1a9
dark0k_ <knezevicdarko899@gmail.com>
2023-01-21 10:55:03 +0100

    mmm

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,9 @@
 function openPlayerConfig() {
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
-  }
-  
-  function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
-  }
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+}
