f1d1c0418dbac0fca318bc6105801f3a93a3d260
dark0k_ <knezevicdarko899@gmail.com>
2023-01-21 10:51:55 +0100

    not trash

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,12 @@
-const configOverlay = document.getElementById("config")
+const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
 
-const editButtonOne = document.getElementById("edit-player-1");
-const editButtonTwo = document.getElementById("edit-player-2");
+const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
+const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 
+editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 
-
-editButtonOne.addEventListener("click", editPlayer);
-editButtonTwo.addEventListener("click", editPlayer);
+cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
